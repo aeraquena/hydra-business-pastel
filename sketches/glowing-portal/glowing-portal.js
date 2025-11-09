88191daf8aa498b3bc2b0eb342495f788f2c0f68
Claire Kwong <clairebkwong@gmail.com>
2025-11-09 18:28:15 -0500

    parameterize glowing portal

--- sketches/glowing-portal/glowing-portal.js
+++ sketches/glowing-portal/glowing-portal.js
@@ -1,11 +1,15 @@
-// https://hydra.ojack.xyz/?code=b3NjKDEzJTJDJTIwMCUyQyUyMDEpJTBBJTIwJTIwLmthbGVpZCgpJTBBJTIwJTIwLm1hc2soc2hhcGUoNCUyQyUyMDAuMyUyQyUyMDEpKSUwQSUyMCUyMC5tb2R1bGF0ZVJvdGF0ZShzaGFwZSg0JTJDJTIwMC4xJTJDJTIwMSkpJTBBJTIwJTIwLm1vZHVsYXRlUm90YXRlKHNoYXBlKDQlMkMlMjAwLjElMkMlMjAwLjkpKSUwQSUyMCUyMC5tb2R1bGF0ZVJvdGF0ZShzaGFwZSg0JTJDJTIwMC4xJTJDJTIwMC44KSklMEElMjAlMjAuc2NhbGUoMC4zKSUwQSUyMCUyMC5hZGQoc2hhcGUoNCUyQyUyMDAuMiUyQyUyMDEpLmNvbG9yKDAuMyUyQyUyMDElMkMlMjAxJTJDJTIwMC41KSklMEElMjAlMjAucm90YXRlKCgpJTIwJTNEJTNFJTIwdGltZSklMEElMjAlMjAub3V0KCklM0IlMEE%3D
-osc(13, 0, 1)
+osc(
+  () => cc[getKnob(1, 1)] * 10,
+  0,
+  () => cc[getKnob(1, 3)]
+)
   .kaleid()
-  .mask(shape(4, 0.3, 1))
+  .mask(shape(() => cc[getKnob(1, 4)] * 5, 0.3, 1))
   .modulateRotate(shape(4, 0.1, 1))
   .modulateRotate(shape(4, 0.1, 0.9))
   .modulateRotate(shape(4, 0.1, 0.8))
-  .scale(0.3)
+  .scale(cc[getKnob(1, 2)] * 10)
   .add(shape(4, 0.2, 1).color(0.3, 1, 1, 0.5))
   .rotate(() => time)
+  .scale(() => cc[getKnob(1, 5)])
   .out();
